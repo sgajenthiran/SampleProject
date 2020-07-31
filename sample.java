1835ddb49ffd14a7e68bb3725def1c16dd857676
sathishkumar gajenthiran <sathishkumar.gajenthiran@igt.com>
2020-07-31 12:55:41 +0530

    modified

--- sample.java
+++ sample.java
@@ -2,11 +2,8 @@ public class sample{
 
 	public static void main(String args[]){
 
-		system.out.println("test");
 		system.out.println("Login");
-		system.out.println("Login1");
 		system.out.println("Register");
-		system.out.println("Register1");	
 
 	}
 
